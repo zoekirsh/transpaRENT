549dae49767bf85983223d6fa9faa42333233cbf
Zoe Kirsh <zoekirsh@gmail.com>
2021-03-10 14:08:29 -0800

    footer functional! open links in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,21 @@ function Footer() {
     <footer className="footer">
       <div className="icon-container center">
         <div className="footer-icons">
-          <span className="footer-icon"><Icon name="github"/></span>
-          <span className="footer-icon"><Icon name="medium"/></span>
-          <span className="footer-icon"><Icon name="linkedin"/></span>
+          <span className="footer-icon">
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/zoekirsh">
+              <Icon name="github"/>
+            </a>
+          </span>
+          <span className="footer-icon">
+            <a target="_blank" rel="noopener noreferrer" href="https://zoekirsh.medium.com/">
+              <Icon name="medium"/>
+            </a>
+          </span>
+          <span className="footer-icon">
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/zoe-kirsh-5792a35b/">
+              <Icon name="linkedin"/>
+            </a>
+          </span>
         </div>
       </div>
     </footer>
